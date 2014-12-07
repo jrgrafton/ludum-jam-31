2ce8e937440169949060d7a9660a971887bd01c1
Tony Wu <tonytlwu@gmail.com>
2014-12-07 12:18:32 +0000

    Character test image updated

--- js/Preloader.js
+++ js/Preloader.js
@@ -14,7 +14,7 @@ AAO.Preloader.prototype.preload = function() {
 
   // Load sprites
   this.load.image('bullet', 'img/bullet.jpg');
-  this.load.image('player', 'img/player.jpg');
+  this.load.image('player', 'img/player.png');
   this.load.spritesheet('zombie', 'img/zombie.png', 128, 128, 7);
   
   this.load.image('background', 'img/background.jpg');
